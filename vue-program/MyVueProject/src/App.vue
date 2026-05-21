5ef75b4fec917e142d04dea76f8ddbc787349548
wyji0809 <1173768536@qq.com>
2026-05-21 17:08:17 +0800

    vue的项目

---
+++ vue-program/MyVueProject/src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>Hello World</h1>
+    <Test/>
+  </div>
+</template>
+
+<script setup>
+// 导入组件
+import Test from './components/Test.vue'
+</script>
---
+++ vue-program/MyVueProject/src/components/Test.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="box">我是自定义组件</div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+.box{border:1px solid #ccc;padding:10px;}
+</style>
